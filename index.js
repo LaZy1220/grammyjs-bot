982a339475d7e4e331931bd5ac714ff96dba2e77
LaZy1220 <4elove4iska@gmail.com>
2024-03-05 12:36:58 +0300

    error catch

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Bot } = require("grammy");
+const { Bot, GrammyError, HttpError } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
@@ -9,6 +9,19 @@ bot.command("start", async (ctx) => {
 
 bot.on("message", async (ctx) => {
   await ctx.reply("Нужно подумать...");
+});
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error(`Error in request`, e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram", e);
+  } else {
+    console.log("Unknown error", e);
+  }
 });
 
 bot.start();
